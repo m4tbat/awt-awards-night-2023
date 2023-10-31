6847cf27cd3b9552e09142114d2aeae392dbd81d
Matteo Battaglio <matteo.battaglio@gmail.com>
2023-10-31 16:01:10 +0100

    debug

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ function setCookie(cname, cvalue, exdays) {
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    console.log("Cookie settato: " + cname + "=" + cvalue + ";" + expires + ";path=/");
 }
 
 function getCookie(cname) {
